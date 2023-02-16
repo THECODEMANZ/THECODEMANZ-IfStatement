c443e30970cdab687972cecf773cf0f950e3c999
THECODEMANZ <kewabailey@gmail.com>
2023-02-16 19:55:51 +0000

    Completed Lab If statement

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -22,9 +22,11 @@ public class Lab {
     public int ifExample(boolean bool, int x, int y){
         if (bool == true) {
         
+
             return x;
         } else { 
         
+            
             return y;
         }
         
--- src/test/java/IfStatementTest.java
+++ src/test/java/IfStatementTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 
 public class IfStatementTest {
     static Lab is;
+    
     @BeforeClass
+
     public static void setUp(){
         is = new Lab();
     }
@@ -16,7 +18,9 @@ public class IfStatementTest {
     @Test
     public void ifStatementTest1(){
         boolean bool = true;
+
         int x = 0;
+
         int y = 1;
         int expected = 0;
         int actual = is.ifExample(bool, x, y);
@@ -30,8 +34,11 @@ public class IfStatementTest {
         boolean bool = false;
 
         int x = 0;
+
         int y = 1;
+
         int expected = 1;
+
         int actual = is.ifExample(bool, x, y);
         Assert.assertEquals(expected, actual);
     }
